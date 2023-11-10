c2b0bd966d4b02edc2d2ee8b605f05439bc92feb
charlesoller <charlesrello@gmail.com>
2023-11-09 19:53:44 -0500

    finished

--- bfs-path.js
+++ bfs-path.js
@@ -1,24 +1,67 @@
 function findNeighbors(node, matrix) {
+    let res = [];
+    let x = node[0]
+    let y = node[1];
+
     // Up
+    let up = [x, y-1]
+    if(matrix[x][y-1]) res.push(up)
 
     // Down
+    let down = [x, y+1]
+    if(matrix[x][y+1]) res.push(down)
 
     // Left
+    let left = [x-1, y];
+    if(matrix[x-1]) res.push(left)
 
     // Right
+    let right = [x+1, y];
+    if(matrix[x+1]) res.push(right)
 
-    // Your code here 
+    // console.log(res)
+    return res;
 }
 
 
 function bfsPath(matrix, startNode, endValue) {
-    // Your code here 
+    // Your code here
+    console.log("START", startNode)
+    let queue = [startNode];
+    let path = [];
+    let visited = new Set();
+    visited.add(`${startNode[0]}, ${startNode[1]}`)
+
+    while(queue.length){
+        console.log(queue)
+        console.log(visited)
+        let node = queue.shift();
+        path.push(node);
+
+        // node = [x, y]
+        // node's value in matrix = matrix[x][y]
+
+        if(matrix[node[0]][node[1]] === endValue){
+            return path;
+        }
+
+        let neighbors = findNeighbors(node, matrix);
+        for(let i = 0; i < neighbors.length; i++){
+            if(!visited.has(`${neighbors[i][0]}, ${neighbors[i][1]}`)){
+                queue.push(neighbors[i])
+                console.log("NEIGHBOR", neighbors[i])
+                visited.add(`${neighbors[i][0]}, ${neighbors[i][1]}`)
+            }
+        }
+    }
+
+    return false;
 }
 
 
 // ***** UNCOMMENT FOR LOCAL TESTING *****
 
-// const matrix1 = [ 
+// const matrix1 = [
 //     [  1,  2,  3,  4 ],
 //     [  5,  6,  7,  8 ],
 //     [  9, 10, 11, 12 ],
@@ -60,11 +103,11 @@ function bfsPath(matrix, startNode, endValue) {
 // // value is located at start node
 // // [ [ 2, 2 ] ]
 
-// console.log(bfsPath(matrix1, [1,2], 8)); // can handle various start nodes 
+// console.log(bfsPath(matrix1, [1,2], 8)); // can handle various start nodes
 // // and end values
 // // [ [ 1, 2 ], [ 0, 2 ], [ 2, 2 ], [ 1, 1 ], [ 1, 3 ] ]
 
-// console.log(bfsPath(matrix1, [0,0], 17)); // can return false if end value 
+// console.log(bfsPath(matrix1, [0,0], 17)); // can return false if end value
 // // is not found
 // // false
 
